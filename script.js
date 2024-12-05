15cf9f052dbccafd17132f6174ba91f3c679cc29
PimvBuren <6043915@mborijnland.nl>
2024-12-05 12:35:15 +0100

    test 6

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Selecteer het inputveld
-const inputField = document.querySelector("116039232"); // Vervang #jouwInputID door de echte ID of class van je inputveld
+const inputField = document.querySelector("116038510"); // Vervang #jouwInputID door de echte ID of class van je inputveld
 
 // Voeg een eventlistener toe
-inputField.addEventListener("116039232", function () {
+inputField.addEventListener("116038510", function () {
     // Vervang punten door komma's
     this.value = this.value.replace(/\./g, ",");
 });
