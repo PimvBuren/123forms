9250a17424c6d73d53d64bb6a55c6f67dc106a8d
PimvBuren <6043915@mborijnland.nl>
2024-12-05 12:21:34 +0100

    test 3
    kijken of deze code werkt

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-function EditEuro() {
-    var Euro={prijs2};
-    
-    InputEuro = document.euroconv.Euro.value;
-    FixedEuro = parseFloat(InputEuro.replace
-    (',',g, '.'));
-    document.euroconv.Mark.value = cent(parseFloat
-    (FixedEuro*KursMark*100)/100);
+function coma(){
+    KommadEuro += '';
+    document.euroconv.Mark.value = KommadEuro.replace('.',g,",");
     }
-    
-    
-    // hier probeer ik de output op het scherm weer te
-    //geven
-    // document.writeln("Input: ",InputEuro,"<br>");
-    
-    //weergeven van resultaat
-    // document.writeln("Output: ", Euro, "<br>");
