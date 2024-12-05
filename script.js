8417a6cfd341edba2f2c690aaf466e93d0a05985
PimvBuren <6043915@mborijnland.nl>
2024-12-05 12:26:18 +0100

    test4
    test 4

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function coma(){
-    KommadEuro += '';
-    document.euroconv.Mark.value = KommadEuro.replace('.',g,",");
-    }
+// Selecteer het inputveld
+const inputField = document.querySelector("#116038510"); // Vervang #jouwInputID door de echte ID of class van je inputveld
+
+// Voeg een eventlistener toe
+inputField.addEventListener("116038510", function () {
+    // Vervang punten door komma's
+    this.value = this.value.replace(/\./g, ",");
+});
