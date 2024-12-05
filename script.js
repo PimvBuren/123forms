7f4d1231d453f5034a22e579f9d6f05d1a5466a7
PimvBuren <6043915@mborijnland.nl>
2024-12-05 12:31:17 +0100

    test 5

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Selecteer het inputveld
-const inputField = document.querySelector("#116038510"); // Vervang #jouwInputID door de echte ID of class van je inputveld
+const inputField = document.querySelector("116038510"); // Vervang #jouwInputID door de echte ID of class van je inputveld
 
 // Voeg een eventlistener toe
 inputField.addEventListener("116038510", function () {
